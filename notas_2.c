6236ff412554e02e6bf1d3e67003ed274ee1dbda
PedroNKx <kerber@alunos.utfpr.edu.br>
2025-04-10 16:37:10 -0300

    Create notas_2.c

---
+++ notas_2.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main ()
+{
+    float n1, n2, n3;
+    float media;
+    int frequencia;
+
+    printf ("Digite as tres notas: ");
+    scanf("%f %f %f", &n1, &n2, &n3);
+
+    media = (n1 + n2 + n3) / 3;
+
+
+    printf ("Digite sua frequencia: ");
+    scanf("%d", &frequencia);
+
+    if (media > 8 && frequencia >= 75)
+        printf("O estudante foi Aprovado com distincao pois teve media %.2f e frequencia %d%%.\n", media, frequencia);
+    else if (media >= 6 && frequencia >=75)
+        printf ("O estudante foi Aprovado Direto pois teve media = %.1f e frequencia = %d%%.\n", media, frequencia);
+    else if ((media >= 4 && media < 6 && frequencia >= 75) ||
+         (media >= 6 && frequencia < 75) ||
+         (media >= 4 && frequencia > 50 && frequencia < 75))
+        printf("O estudante foi Vai para Final pois teve media %.1f e frequencia %d%%.\n", media, frequencia);
+    else
+        printf ("O estudante foi Reprovado Direto pois teve media %.1f e frequencia %d%%.\n", media, frequencia);
+        return 0;
+}
