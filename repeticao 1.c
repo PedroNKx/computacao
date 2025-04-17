a9289fb365f16d21aa29d6255fc2131765cbddfb
PedroNKx <kerber@alunos.utfpr.edu.br>
2025-04-17 17:12:20 -0300

    repeticao 1.c

--- repeticao 1.c
+++ repeticao 1.c
@@ -7,25 +7,24 @@ int main()
 
     printf("Digite numeros inteiros. Para encerrar, digite um numero negativo.\n");
 
-
     printf("Digite um numero: ");
     scanf("%d", &numero);
 
-    if (numero < 0)
-    {
-        printf("Voce nao digitou nenhum numero positivo.\n");
-        return 0;
-    }
-    printf("Digite um numero: ");
-    scanf("%d", &numero);
+    maior = numero;
+
+
     while (numero >= 0)
     {
         if (numero > maior)
         {
             maior = numero;
         }
+
+        printf("Digite um numero: ");
+        scanf("%d", &numero);
     }
 
     printf("O maior numero digitado foi: %d\n", maior);
     return 0;
 }
+
