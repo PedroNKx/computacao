03ca673d603d727b49204807a77ec411037206f7
PedroNKx <kerber@alunos.utfpr.edu.br>
2025-04-24 16:42:07 -0300

    Update repeticao 1.c

--- repeticao 1.c
+++ repeticao 1.c
@@ -1,30 +1,25 @@
 #include <stdio.h>
 
-int main()
-{
-    int numero;
-    int maior;
+int main() {
+    int base, inicio, fim, soma = 0, i;
 
-    printf("Digite numeros inteiros. Para encerrar, digite um numero negativo.\n");
+    printf("Digite o numero base: ");
+    scanf("%d", &base);
+    printf("Digite o valor inicial do intervalo: ");
+    scanf("%d", &inicio);
+    printf("Digite o valor final do intervalo: ");
+    scanf("%d", &fim);
 
-    printf("Digite um numero: ");
-    scanf("%d", &numero);
-
-    maior = numero;
-
-
-    while (numero >= 0)
-    {
-        if (numero > maior)
-        {
-            maior = numero;
+    i = inicio;
+    while (i <= fim) {
+        if (i % base == 0) {
+            soma += i;
         }
-
-        printf("Digite um numero: ");
-        scanf("%d", &numero);
+        i++;
     }
 
-    printf("O maior numero digitado foi: %d\n", maior);
+    printf("Soma dos multiplos de %d entre %d e %d: %d\n", base, inicio, fim, soma);
+
     return 0;
 }
 
